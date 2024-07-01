ea683d2b001e25c2ee2291b0361b65b13de48987
franck-tchassi <franck.tchassi@laplateforme.io>
2024-07-01 14:43:37 +0200

    gestion des erreurs

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,54 @@
+
 const http = require('http');
-const app = require('./app.js');
+const app = require('./app');
 
-const normalizePort = val =>{
-    
-    const port = parseInt(val, 10);
 
-    if(isNaN(port)){
-        return val;
-    }
-    if (port >= 0){
-       return port;
-    }
+//renvoie un port valide
+const normalizePort = val => {
+  const port = parseInt(val, 10);
 
-    return false;
-}
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
 
-const port = normalizePort(process.env.PORT || 4000)
+const port = normalizePort(process.env.PORT || '4000');
+app.set('port', port);
 
-const server = http.createServer(app)
 
-server.listen(port, ()=>{
-    console.log("server is runned !!!")
-})
+//Eventlistener gèrant les erreurs
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+//ecouteur port
+server.listen(port);
