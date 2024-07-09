ae69b97b9a16ac06cf465eb94b7108cd67d30335
franck-tchassi <115140772+franck-tchassi@users.noreply.github.com>
2024-07-09 09:09:00 +0200

    Ajout des routes pour la gestion des livres, y compris la notation

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router()
+const auth = require('../middleware/auth');
+const { upload, resizeImage } = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/book')
 
 
-
-//route qui affiche tous les livres
-router.get("/books", bookCtrl.getAllBook)
-
-router.post("/books", bookCtrl.createBook)
+router.get('/bestrating', bookCtrl.bestRating);
+router.get('/', bookCtrl.library);
+router.get('/:id', bookCtrl.oneBook);
+router.post('/', auth, upload, resizeImage, bookCtrl.createBook);
+router.put('/:id', auth, upload, resizeImage, bookCtrl.updateBook );
+router.delete('/:id', auth, bookCtrl.deleteBook);
+router.post('/:id/rating', auth, bookCtrl.rateBook);
 
 
 
