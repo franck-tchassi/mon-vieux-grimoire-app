a11e6c684870d901be5f05c6a17e326f44a602d3
franck-tchassi <115140772+franck-tchassi@users.noreply.github.com>
2024-07-09 09:34:34 +0200

    Ajout le controller createBook pour créer un livre

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -16,6 +16,25 @@ exports.oneBook = (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 };
 
+//post - ajoute un livre
+exports.createBook =  (req, res, next)=>{
+    const bookObject = JSON.parse(req.body.book);
+    // Suppression du faux _id envoyé par le front
+    delete bookObject._id;
+    // Suppression de _userId auquel on ne peut faire confiance
+    delete bookObject._userId;
+    // Création d'une instance du modèle Book
+    const book = new Book({
+        ...bookObject,
+        userId: req.auth.userId,
+        imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    })
+    // Enregistrement dans la base de données
+    book.save()
+    .then(()=>{res.status(200).json({message: 'un livre enregistré !!'})})
+    .catch((error)=>{res.status(400).json({error})})
+}
+
 
 
 
