819593fd7f844bb2a1026d640566d476db4f88ee
franck-tchassi <115140772+franck-tchassi@users.noreply.github.com>
2024-07-27 08:32:48 +0200

    mise en place du token secret dans un fichier d'environnement

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
   try {
     // Correctly split the Authorization header
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'TOKEN_SECRET');
+    const decodedToken = jwt.verify(token, process.env.JWT_BODY_TOKEN);
     const userId = decodedToken.userId;
     req.auth = { userId: userId };
     next();
@@ -13,19 +13,4 @@ module.exports = (req, res, next) => {
   }
 };
 
-
-module.exports = (req, res, next) =>{
-    try{
-       const token = req.headers.authorization.split(' ')[1];
-       const decodedToken = jwt.verify(token, 'TOKEN_SECRET');
-       const userId = decodedToken.userId;
-       req.auth = {
-        userId: userId
-       }
-       next()
-    }
-    catch(error){
-        res.status(401).json({error});
-    }
-}
 
